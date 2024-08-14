4fe1d142e2e31a253272bb223191c2eb9e804ceb
William Baker <bbrk24@gmail.com>
2024-08-14 12:49:59 -0400

    Saving my work before lunch

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,9 +1,20 @@
 import * as fs from 'node:fs/promises';
 import * as esbuild from 'esbuild';
+import yaml from 'js-yaml';
 import heraPlugin from '@danielx/hera/esbuild-plugin';
 import civetPlugin from '@danielx/civet/dist/esbuild-plugin.js';
 
-await fs.mkdir('dist/img', { recursive: true });
+async function compileYaml(filename) {
+    const file = await fs.readFile(filename, { encoding: 'utf-8' });
+    const object = yaml.load(file);
+    const json = JSON.stringify(object);
+    await fs.writeFile(filename.replace(/\.ya?ml$/, '.json'), json);
+}
+
+await Promise.all([
+    fs.mkdir('dist/img', { recursive: true }),
+    compileYaml('src/translation/dictionary.yml'),
+]);
 await Promise.all([
     esbuild.build({
         entryPoints: ['src/index.civet'],
